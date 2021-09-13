6fbe880b52e219d5c3264079109d374ab4724b1e
nashvador <90426925+nashvador@users.noreply.github.com>
2021-09-13 16:54:08 -0500

    Day 4 of learning python, attached is a rock-paper-scissors game
    I've explored two ways of doing these, first way gives the result (if you won or not) through conditional statements. Second is through the matrix through lists.

---
+++ RockPaperScissors.py
@@ -0,0 +1,54 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+game = [rock, paper, scissors]
+print(game[choice])
+game1 = random.randint(0,2)
+print(f"Computer chose\n {game[game1]}") 
+
+if choice == 0:
+  if game1 == 0:
+    print("Draw!")
+  elif game1 == 1:
+    print("You lose!")
+  else:
+    print("You win!")
+elif choice == 1:
+  if game1 == 0:
+    print("You win!")
+  elif game1 == 1:
+    print("Draw!")
+  elif game1 == 2:
+    print("You lose!")
+else:
+  if game1 == 0:
+    print("You lose!")
+  elif game1 == 1:
+    print("You win!")
+  else:
+    print("Draw!")
